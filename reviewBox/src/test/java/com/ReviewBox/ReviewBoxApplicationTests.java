42f33a24b126eee1ebdab45f30ba2d2c00b5bf8e
Chandru J <86970969+chandru-03@users.noreply.github.com>
2025-03-14 11:41:44 +0530

    Add files via upload

---
+++ reviewBox/src/test/java/com/ReviewBox/ReviewBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReviewBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
